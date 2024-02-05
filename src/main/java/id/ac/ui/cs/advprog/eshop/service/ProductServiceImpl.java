62f7ffc0dfbda412f822afbfba44687ffcccb930
Kristoforus Adi Himawan <kristoforus.adi28@gmail.com>
2024-02-05 18:52:25 +0700

    feat: implement edit in repository and service files

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -22,10 +22,20 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    public Product edit(String productId, Product product) {
+        return productRepository.edit(productId, product);
+    }
+
+    @Override
     public List<Product> findAll() {
         Iterator<Product> productIterator = productRepository.findAll();
         List<Product> allProduct = new ArrayList<>();
         productIterator.forEachRemaining(allProduct::add);
         return allProduct;
+    }
+
+    @Override
+    public Product findById(String productId) {
+        return productRepository.findById(productId);
     }
 }

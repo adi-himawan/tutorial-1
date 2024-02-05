a838efc3cd66d4430c7e7aba827b800c9206326a
Kristoforus Adi Himawan <kristoforus.adi28@gmail.com>
2024-02-05 19:45:14 +0700

    feat: implement delete in repository and service files

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -10,10 +10,21 @@ import java.util.List;
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
+    private long productCount = 0;
 
     public Product create(Product product) {
+        product.setProductId(String.valueOf(++productCount));
         productData.add(product);
         return product;
+    }
+
+    public void delete(String productId) {
+        for (Product product: productData) {
+            if (productId.equals(product.getProductId())) {
+                productData.remove(product);
+                break;
+            }
+        }
     }
 
     public Iterator<Product> findAll() {
